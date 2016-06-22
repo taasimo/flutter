8015a261806096afbcb85050e4f7c139ee0ef145
Taasimo <taasimo2@gmail.com>
2016-06-22 14:24:29 +0000

    generated Pages index home explore profile, root changes

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,19 @@
+# the pages controller contains all of the code any page inside of /pages 
+class PagesController < ApplicationController
+  
+  # Back-end code for pages/index
+  def index
+  end
+
+  # Back-end code for pages/home
+  def home
+  end
+
+  # Back-end code for pages/profile
+  def profile
+  end
+
+  # Back-end code for pages/explore
+  def explore
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
+  
+  
+  # define root URL
+  root 'pages#index'
+  # define routes for pages
+
+
+  get '/home' => 'pages#home' # overrides default pages
+  get '/profile' => 'pages#profile'
+  get '/explore' => 'pages#explore'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
