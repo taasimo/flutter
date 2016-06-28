a6d3c7d528c6ab2eb98436a8c5515c1c6ddd2789
Taasimo <taasimo2@gmail.com>
2016-06-28 10:03:52 +0000

    Finishing GUI

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,6 +8,7 @@ class PagesController < ApplicationController
   # Back-end code for pages/home
   def home
      @posts = Post.all
+       @newPost = Post.new 
   end
 
   # Back-end code for pages/profile
@@ -22,10 +23,14 @@ class PagesController < ApplicationController
   
     @posts = Post.all.where("user_id = ?", User.find_by_username(params[:id]).id)
     @newPost = Post.new 
+    
+    @toFollow = User.all.last(5)
 end
 
   # Back-end code for pages/explore
   def explore
      @posts = Post.all
+     @newPost = Post.new
+     @toFollow = User.all.last(5)
   end
 end
