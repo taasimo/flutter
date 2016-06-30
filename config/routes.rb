78e6d2dadb603b3bc8b484a3dbf71f685ece0871
Taasimo <taasimo2@gmail.com>
2016-06-30 07:55:48 +0000

    Added follow and unfollow buttons

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,18 @@ Rails.application.routes.draw do
   
   
   devise_for :users
-  
+  resources :users do 
+    member do
+      get :following, :followers
+    end
+  end
+  resources :relationships, only: [:create, :destroy]
   resources :posts
   # define root URL
   root 'pages#index'
+  
+  
   # define routes for pages
-
-
   get '/home' => 'pages#home' # overrides default pages
   get '/user/:id' => 'pages#profile'
   get '/explore' => 'pages#explore'
